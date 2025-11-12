6a32838dd427a0fa05b3c33c8e20c87ada16233e
akshu41-amazon <rjakshay@amazon.com>
2025-11-12 16:59:45 +0000

    Update static-test.vue

--- pages/static-test.vue
+++ pages/static-test.vue
@@ -4,7 +4,7 @@
     <p>This page should be prerendered as static HTML.</p>
     <p>If you can see this after refreshing the page at /static-test, then the issue is resolved.</p>
     <p>If you get a 502 error when refreshing at /static-test, then the issue is reproduced.</p>
-    <p>This page will work after refresh</p>
+    <p>This page will work after refresh h</p>
     <NuxtLink to="/">Back to Home</NuxtLink>
   </div>
 </template>
